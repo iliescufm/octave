fd48e35d5b7a0f06df6750d9955f08c4d0712cf1
Mihai Iliescu <iliescufm@yahoo.com>
2023-12-01 15:37:53 +0200

    command line in Validate.c

--- Validate.c
+++ Validate.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #define MODULO 10007
+# define L 1
+# define D 2
+# define P 3
+# define C 4
+# define M 5
+# define O 6
+# define T 7
+# define R 8
+# define F 9
+# define Q 0
+# define S 11
 
 int validate_index(int k, int size)
 {
@@ -30,4 +41,66 @@ int modulo_positive(int s)
 	if (s < 0)
 		s = s + MODULO;
 	return s;
+}
+
+int get(int qz, int* k, int *p, int *lx, int *li, int *cx, int *ci)
+{
+	char key = '\0';
+	int ok = 0;
+	scanf("%c", &key);
+	if (key == 'L') {
+		scanf("%d%d", lx, cx); return L;
+	}
+	if (key == 'D') {
+		ok = 1;
+		scanf("%d", k);
+		if (validate_index(*k, qz)) return D;
+	}
+	if (key == 'P') {
+		ok = 1; 
+		scanf("%d", k);
+		if (validate_index(*k, qz)) return P;
+	}
+	if (key == 'C') {
+		ok = 1;
+		scanf("%d", k);
+		scanf("%d", lx); // nr of lines
+		for (int i = 0; i < *lx; i++)
+			scanf("%d", &li[i]);
+		scanf("%d", cx); // nr of columns
+		for (int i = 0; i < *cx; i++)
+			scanf("%d", &ci[i]);
+		if (validate_index(*k, qz)) return C;
+		}
+	if (key == 'M') {
+		ok = 1;
+		scanf("%d%d", lx, cx);
+		if (validate_index(*lx, qz) && validate_index(*cx, qz)) return M;
+		}
+	if (key == 'O') return O;
+	if (key == 'T') {
+		ok = 1;
+		scanf("%d", k);
+		if (validate_index(*k, qz)) return T;
+		}
+	if (key == 'R') {
+		ok = 1;
+		scanf("%d%d", k, p);
+		if (validate_index(*k, qz)) return R;
+		}
+	if (key == 'F') {
+		ok = 1;
+		scanf("%d", k);
+		if (validate_index(*k, qz)) return F;
+		}
+	if (key == 'Q') return Q;
+	if (key == 'S') {
+		ok = 1;
+		scanf("%d%d", lx, cx);
+		if (validate_index(*lx, qz) && validate_index(*cx, qz)) return S;
+		}
+	if (key != '\n' && key != '\r') {
+		if (! ok) printf("Unrecognized command\n");
+		}
+	return 99;
 }
--- Validate.h
+++ Validate.h
@@ -1,3 +1,4 @@
 int validate_index(int k, int size);
 int validate_size(int k1, int k2, int *l, int *c);
 int modulo_positive(int r);
+int get(int qz, int* k, int *p, int *lx, int *li, int *cx, int *ci);
